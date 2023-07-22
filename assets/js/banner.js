d9e0a30dbb0943d837b5ae08da0f98985872fde6
jaredgendron <jaredwgendron@gmail.com>
2023-07-22 16:22:05 -0700

    banner changes to display name after a couple seconds

---
+++ assets/js/banner.js
@@ -0,0 +1,23 @@
+function changeText() {
+  var headingElement = document.getElementById('heading');
+  var paragraphElement = document.getElementById('paragraph');
+
+  // Fade out the elements by setting opacity to 0
+  headingElement.style.opacity = 0;
+  paragraphElement.style.opacity = 0;
+
+  // After a short delay (e.g., 100 milliseconds), change the text and fade in
+  setTimeout(function() {
+    var newText = "Jared <span class='gradient1'>Gendron</span>";
+
+    headingElement.innerHTML = newText;
+    paragraphElement.textContent = "Electrical Engineering Graduate, Vancouver BC";
+
+    // Fade in the elements by setting opacity to 1
+    headingElement.style.opacity = 1;
+    paragraphElement.style.opacity = 1;
+  }, 500);
+}
+
+// Execute the changeText() function after 3 seconds (3000 milliseconds) of page load
+setTimeout(changeText, 3000);
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,6 +2,7 @@
 function scrollHeader(){
   const header = document.getElementById('header')
   // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-  if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+  if(this.scrollY >= 250) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
+
 window.addEventListener('scroll', scrollHeader)
