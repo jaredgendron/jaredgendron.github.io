340107afcb73e201d1e43a7d369d929f43217157
jaredgendron <jaredwgendron@gmail.com>
2023-07-30 12:16:08 -0700

    fixed portfolio page

--- assets/js/expand.js
+++ assets/js/expand.js
@@ -1,10 +1,34 @@
-// Title section click event
-document.addEventListener('click', function (event) {
-  const target = event.target;
-  const titleSection = target.closest('.title-section');
+const projectLinks = document.querySelectorAll('.project-link');
+const projectTitle = document.getElementById('project-title');
+const projectSummary = document.getElementById('project-summary');
+const projectDescription = document.getElementById('project-description');
+const projectImage = document.getElementById('project-image');
 
-  if (titleSection) {
-    const project = titleSection.parentElement;
-    project.classList.toggle('expanded');
-  }
+function handleProjectClick(event) {
+  const selectedProjectTitle = event.target.dataset.title;
+  const selectedProject = findProjectByTitle(selectedProjectTitle);
+  projectTitle.innerText = selectedProject.title;
+  projectSummary.innerText = selectedProject.summary;
+  projectDescription.innerText = selectedProject.description;
+  projectImage.src = selectedProject.image;
+}
+
+projectLinks.forEach(link => {
+  link.addEventListener('click', handleProjectClick);
 });
+
+function findProjectByTitle(title) {
+  for (const project of projects) {
+    if (project.title === title) {
+      return project;
+    }
+  }
+  return null;
+}
+
+if (projects.length > 0) {
+  projectTitle.innerText = projects[0].title;
+  projectSummary.innerText = projects[0].summary;
+  projectDescription.innerText = projects[0].description;
+  projectImage.src = projects[0].image;
+}
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,6 +2,7 @@ var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('header').outerHeight();
+const sidebar = document.querySelector('wrapper');
 
 $(window).scroll(function(event){
   didScroll = true;
@@ -22,9 +23,15 @@ function hasScrolled() {
   
   if (st > lastScrollTop && st > navbarHeight){
     $('header').addClass('hidden');
+    if(sidebar){
+      sidebar.classList.addclass('full');
+    }
   } else {
     if(st + $(window).height() < $(document).height()) {
       $('header').removeClass('hidden');
+      if(sidebar){
+        sidebar.classList.removeclass('full');
+      }
     }
   }
   
