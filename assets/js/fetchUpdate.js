6d98099365697ac271d78b772d0c5bcf6f2379ca
jaredgendron <jaredwgendron@gmail.com>
2023-09-01 17:07:12 -0700

    updated js and css

--- assets/js/fetchUpdate.js
+++ assets/js/fetchUpdate.js
@@ -10,15 +10,47 @@ updateTimeSpan.textContent = "Fetching...";
 fetch(branchUrl)
   .then(response => response.json())
   .then(branchData => {
-    // Extract commit date and time value from commit data
-    const commitDate = branchData.commit.commit.committer.date;
+    // Extract commit date from commit data
+    const commitDate = new Date(branchData.commit.commit.committer.date);
 
-    // Format the date and time (optional)
-    const formattedDate = new Date(commitDate).toLocaleString();
+    // Get the current date
+    const currentDate = new Date();
+
+    // Calculate the time difference in milliseconds
+    const timeDiff = currentDate - commitDate;
+
+    // Calculate days difference
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    // Define strings for "today" and "yesterday"
+    const todayString = "today";
+    const yesterdayString = "yesterday";
+
+    let timeAgo;
+
+    // Check if it's today
+    if (daysDiff === 0) {
+      timeAgo = todayString;
+    }
+    // Check if it's yesterday
+    else if (daysDiff === 1) {
+      timeAgo = yesterdayString;
+    }
+    // Otherwise, display "# days ago"
+    else {
+      timeAgo = daysDiff + " days ago";
+    }
+
+    // Format the date and time to display without seconds
+    const options = { hour: 'numeric', minute: 'numeric' };
+    const formattedDate = commitDate.toLocaleString(undefined, options);
+
+    // Combine the timeAgo and formattedDate
+    const updateTimeText = `${timeAgo}`;// at ${formattedDate}`;
 
     // Update the content of the span element
-    updateTimeSpan.textContent = formattedDate;
+    updateTimeSpan.textContent = updateTimeText;
   })
   .catch(error => {
-    updateTimeSpan.textContent = "Could not fetch latest update timestamp at this time";
+    updateTimeSpan.textContent = "Could not fetch the latest update timestamp at this time";
   });
